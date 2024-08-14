dfe9e5cb33a601a3a94ab850e7a69162227fc694
karthickvkumar <karthickvelraj@live.in>
2024-08-14 09:00:42 +0530

    commit

---
+++ programming.js
@@ -0,0 +1,43 @@
+/* JavaScript - Case Senstivive
+
+Data Type (In this we will learn how to priovide input to JavaScript)
+1. Primitive Type (pre Defined Type)
+
+a) Number - Any Numerical value include Positive, Negative, decimal, fractional 
+b) String - Any text value which is enclosed inside a single or double Quote '' ""
+c) Boolean - true , false 
+d) null - Non exisit value 
+e) undefined - It has been declared but not assigned any value
+
+2. Object Type (User Defined Type)
+
+a) Object - It is a collection of key and value pair 
+b) Array - It is a List of data 
+c) Function - It is a block of Code */
+
+/*
+To pass a input to Javascript:
+Variable - It a temp storage to hold the input (data) 
+syntax:
+var VariableName = Value;
+
+var - Keyword use to create variable 
+VariableName - Name for the temp storage (Name of the variable)
+    - Should contains only A-Z, a-z, 0-9, _ , $
+    - User defined Name 
+Value - The input what we are going to pass 
+*/
+
+var user_age = 25;
+var user_age_result = user_age * 2;
+console.log(user_age_result)
+
+/*
+To show out to User:
+1. console.log()
+2. alert()
+3. Printing into HTML 
+*/
+
+var user_email_id = "karthick1232@gmail.com";
+console.log(user_email_id);
