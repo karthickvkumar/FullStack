98069eb0d620397ea9fbdcbeaddc5273ea4fb597
karthickvkumar <karthickvelraj@live.in>
2024-08-19 08:51:47 +0530

    commit message

--- programming.js
+++ programming.js
@@ -41,3 +41,64 @@ To show out to User:
 
 var user_email_id = "karthick1232@gmail.com";
 console.log(user_email_id);
+
+var passResult = true;
+console.log(passResult);
+
+var upcommingValue;
+console.log(upcommingValue);
+
+var firstName = "mr.abc";
+var lastName = "xyz";
+var rollNumber = "2344ADC53534534534";
+var contatNumber = 987558268556;
+
+/*
+To Create an Object 
+syntax:
+var VariableName = { 
+    key1 : value,
+    key2 : value
+};
+
+key - similar to Variable Name, it should be unique in a variable
+*/
+
+var studentDetails = {
+    firstName : "mr.abc",
+    lastName : "xyz",
+    rollNumber : "2344ADC53534534534",
+    contatNumber : 987558268556
+};
+
+console.log(studentDetails);
+
+/*
+To create an Array
+syntax: 
+var VariableName = [ value1, value2, value3, ... valueN ];
+
+value1 - Input (Number type)
+*/
+
+var scoreList = [20, 30, 45, 10, 55];
+
+var nameList = ["abc", "cde", "efg", "hij"];
+
+var studentDetailList = [
+    {
+        firstName : "mr.abc",
+        lastName : "xyz",
+        rollNumber : "2344ADC53534534534",
+        contatNumber : 987558268556
+    },
+    {
+        firstName : "mr.wer",
+        lastName : "poi",
+        rollNumber : "434535343DEDDF5556",
+        contatNumber : 784555555
+    }
+];
+
+console.log(nameList);
+console.log(studentDetailList);
