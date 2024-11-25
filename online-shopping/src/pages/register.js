135fec13d1d6e59ec81a5a82e899c321675a1904
karthickvkumar <karthickvelraj@live.in>
2024-11-25 08:57:48 +0530

    commit message

--- online-shopping/src/pages/register.js
+++ online-shopping/src/pages/register.js
@@ -14,6 +14,17 @@ const RegisterPage = () => {
         address: ""
     });
 
+    const [error, updateError] = useState({
+        name : false,
+        email : false,
+        password : false,
+        dateOfBirth : false,
+        gender: false,
+        hobbies: false,
+        qualification: false,
+        address: false
+    });
+
     const copyInputField = (event) => {
         if(event.target.id === "hobbies"){
             if(event.target.checked){
@@ -33,7 +44,18 @@ const RegisterPage = () => {
     }
 
     const registerAction = () => {
-        console.log(registerForm)
+        console.log(registerForm);
+
+        updateError({
+            name : registerForm.name === "" ? true : false,
+            email : registerForm.email === "" ? true : false,
+            password : registerForm.password === "" ? true : false,
+            dateOfBirth : registerForm.dateOfBirth === "" ? true : false,
+            gender: registerForm.gender === "" ? true : false,
+            hobbies: registerForm.hobbies.length === 0 ? true : false,
+            qualification: registerForm.qualification === "" ? true : false,
+            address: registerForm.address === "" ? true : false,
+        })
     }
 
     return(
@@ -43,24 +65,34 @@ const RegisterPage = () => {
             <div className="contact-space">
                 <label className="contact-label">Enter your Name</label>
                 <input id="name" className="contact-textfield" type="text" placeholder="Enter your Name" onChange={copyInputField} />
+
+                { error.name && <span className="error-message">Please enter a valid Name <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Enter your Email Address</label>
                 <input id="email" className="contact-textfield" type="text" placeholder="Enter your Email Address" onChange={copyInputField}/>
+
+                { error.email && <span className="error-message">Please enter a valid Email Address <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Enter your Password</label>
                 <input id="password" className="contact-textfield" type="password" placeholder="Enter your Password"  onChange={copyInputField}/>
+
+                { error.password && <span className="error-message">Please enter a valid Password <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Select your DOB</label>
                 <input id="dateOfBirth" className="contact-textfield" type="date"  onChange={copyInputField}/>
+
+                { error.dateOfBirth && <span className="error-message">Please select a valid Data of Birth <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Select your Gender</label>
                 <input id="gender" type="radio" name="gender" onChange={copyInputField} value="Male"/>Male
                 <input id="gender" type="radio" name="gender" onChange={copyInputField} value="Female"/>Female
                 <input id="gender" type="radio" name="gender" onChange={copyInputField} value="NA"/>NA
+
+                { error.gender && <span className="error-message">Please select any one Gender <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Select your hobbies</label>
@@ -70,6 +102,8 @@ const RegisterPage = () => {
                 <input id="hobbies" type="checkbox" onChange={copyInputField} value="Hockey"/>Hockey
                 <input id="hobbies" type="checkbox" onChange={copyInputField} value="Vollyball"/>Vollyball
                 <input id="hobbies" type="checkbox" onChange={copyInputField} value="Horse Ride"/>Horse Ride
+
+                {error.hobbies && <span className="error-message">Please select any one Hobbies <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Select your Qualification</label>
@@ -80,10 +114,14 @@ const RegisterPage = () => {
                     <option>Degree College</option>
                     <option>Masters College</option>
                 </select>
+
+                { error.qualification && <span className="error-message">Please select any one Qualification <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <label className="contact-label">Address</label>
                 <textarea id="address" className="contact-textfield" onChange={copyInputField}></textarea>
+
+                { error.address && <span className="error-message">Please enter a valid Address <img src={require("../images/warning.png")} width="20"/> </span>}
             </div>
             <div className="contact-space">
                 <button className="contact-submit" onClick={() => registerAction()}>Register</button>
