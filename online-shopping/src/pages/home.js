5e1515c48a5dbc4469a32bf8d6d2e1aa29ca4cf8
karthickvkumar <karthickvelraj@live.in>
2024-12-08 09:12:40 +0530

    commit message

--- online-shopping/src/pages/home.js
+++ online-shopping/src/pages/home.js
@@ -1,12 +1,26 @@
 import React from "react";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
+import axios from "axios";
 
 const HomePage = () => {
+
+    const getInformation = () => {
+        const url = "https://reqres.in/api/users?page=2";
+        axios.get(url)
+            .then((response) => {
+                console.log(response);
+            })
+            .catch((error) => {
+                console.log(error);
+            })
+    }
+
     return(
         <div>
             <HeaderComponent></HeaderComponent>
             <h1>Home Page</h1>
+            <button onClick={() => getInformation()}>GET Method</button>
             {/* <img src={require("../images/shinchan.jpg")} width={200} />
             <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSOJHgZRtno3BK-NtzSZRdxikRnw5N1deYXaw&s" width={200}/> */}
             <FooterComponent></FooterComponent>
